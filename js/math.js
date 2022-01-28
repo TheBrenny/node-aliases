3f60736dfef29349d7c70ce33d163d6b49e87e20
TheBrenny <iam+dev@justbrenny.me>
2022-01-28 11:19:27 +1030

    MATH

---
+++ js/math.js
@@ -0,0 +1,13 @@
+if (process.argv.length == 2) {
+    console.log("Usage: math <...equation>");
+    console.log("  Where equation can be any length of anything that gets piped directly to a JS eval statement. So be careful!");
+} else {
+    let expr = process.argv.slice(2);
+    if (process.argv[2] === "-s") expr = expr.slice(1);
+    expr = expr.join(" ");
+
+    let ans = eval(expr);
+    if (process.argv[2] !== "-s") console.log(`${expr} = \x1b[33m${ans}\x1b[0m`);
+
+    process.exit(ans);
+}

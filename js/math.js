bcb7cf35f198186cd33573ceb3af0a914b922712
TheBrenny <iam@justbrenny.me>
2023-07-08 18:27:09 +1000

    update math

--- js/math.js
+++ js/math.js
@@ -1,14 +1,25 @@
-if (process.argv.length == 2) {
+const {VM} = require("vm2");
+
+if(process.argv.length == 2) {
     console.log("Usage: math <...equation>");
     console.log("  Where equation can be any length of anything that gets piped directly to a JS eval statement. So be careful!");
     console.log("  (Interactive mode coming soon!)");
 } else {
+    // TODO: Use the Node Virtual Machine (VM) module!
     let expr = process.argv.slice(2);
     let silent = process.argv[2] === "-s";
-    if (silent) expr = expr.slice(1);
+    if(silent) expr = expr.slice(1);
 
     expr = expr.join(" ");
-    let ans = eval(expr);
+    // let ans = eval(expr);
+    let ans = new VM({
+        allowAsync: false,
+        timeout: 500,
+        sandbox: {},
+        compiler: "javascript",
+        eval: false,
+        wasm: false,
+    }).run(expr);
 
     if(!silent) process.stdout.write(`${expr} = \x1b[33m`);
     process.stdout.write(ans.toString());

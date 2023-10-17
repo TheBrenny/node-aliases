5202f1dd0806ac8cdcbadef7e29ec16dc737f536
jxbre <jarod.brennfleck@defence.gov.au>
2023-10-18 10:01:46 +1100

    remove math comments

--- js/math.js
+++ js/math.js
@@ -5,13 +5,11 @@ if(process.argv.length == 2) {
     console.log("  Where equation can be any length of anything that gets piped directly to a JS eval statement. So be careful!");
     console.log("  (Interactive mode coming soon!)");
 } else {
-    // TODO: Use the Node Virtual Machine (VM) module!
     let expr = process.argv.slice(2);
     let silent = process.argv[2] === "-s";
     if(silent) expr = expr.slice(1);
 
     expr = expr.join(" ");
-    // let ans = eval(expr);
     let ans = new VM({
         allowAsync: false,
         timeout: 500,

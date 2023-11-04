5813e462dae63e663b19525fbaaa0e9259339352
TheBrenny <iam@justbrenny.me>
2023-11-04 20:01:59 +1100

    fixed math to use isolated-vm

--- js/math.js
+++ js/math.js
@@ -1,26 +1,21 @@
-const {VM} = require("vm2");
-
-if(process.argv.length == 2) {
+if (process.argv.length == 2) {
     console.log("Usage: math <...equation>");
     console.log("  Where equation can be any length of anything that gets piped directly to a JS eval statement. So be careful!");
     console.log("  (Interactive mode coming soon!)");
 } else {
+    const Isolate = require("isolated-vm").Isolate;
+    const iso = new Isolate({ memoryLimit: 8 });
+    const context = iso.createContextSync();
+
     let expr = process.argv.slice(2);
     let silent = process.argv[2] === "-s";
-    if(silent) expr = expr.slice(1);
+    if (silent) expr = expr.slice(1);
 
     expr = expr.join(" ");
-    let ans = new VM({
-        allowAsync: false,
-        timeout: 500,
-        sandbox: {},
-        compiler: "javascript",
-        eval: false,
-        wasm: false,
-    }).run(expr);
+    let ans = context.evalSync(expr, { promise: false });
 
-    if(!silent) process.stdout.write(`${expr} = \x1b[33m`);
+    if (!silent) process.stdout.write(`${expr} = \x1b[33m`);
     process.stdout.write(ans.toString());
-    if(!silent) process.stdout.write(`\x1b[0m`);
+    if (!silent) process.stdout.write(`\x1b[0m`);
     process.stdout.write("\n");
 }

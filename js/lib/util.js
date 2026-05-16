14fd4dc233ad644f3ab3d49a7e44bd3bae790def
TheBrenny <iam@justbrenny.me>
2026-05-16 19:43:44 +0930

    moved normaliseSize

--- js/lib/util.js
+++ js/lib/util.js
@@ -31,3 +31,21 @@ module.exports.tail = function (file, maxLines) {
         rl.on("error", (e) => reject(e));
     });
 }
+
+module.exports.normaliseSize = function (size) {
+    let bytes = size % 1024;
+    let kilobytes = (size / 1024) % 1024;
+    let megabytes = (size / 1024 / 1024) % 1024;
+    let gigabytes = (size / 1024 / 1024 / 1024) % 1024;
+    let terabytes = (size / 1024 / 1024 / 1024 / 1024) % 1024;
+    let petabytes = (size / 1024 / 1024 / 1024 / 1024 / 1024) % 1024;
+
+    let out = [];
+    if (petabytes > 1) return petabytes.toFixed(2) + " PB";
+    if (terabytes > 1) return terabytes.toFixed(2) + " TB";
+    if (gigabytes > 1) return gigabytes.toFixed(2) + " GB";
+    if (megabytes > 1) return megabytes.toFixed(2) + " MB";
+    if (kilobytes > 1) return kilobytes.toFixed(2) + " KB";
+    if (bytes > 1) return bytes.toFixed(2) + " B";
+    return "0 zero size";
+}
--- js/size.js
+++ js/size.js
@@ -48,22 +48,4 @@ const bossman = require("big-kahuna");
         console.error("\"" + t + "\" is not a file or directory!");
         return 0;
     }
-
-    function normaliseSize(size) {
-        let bytes = size % 1024;
-        let kilobytes = (size / 1024) % 1024;
-        let megabytes = (size / 1024 / 1024) % 1024;
-        let gigabytes = (size / 1024 / 1024 / 1024) % 1024;
-        let terabytes = (size / 1024 / 1024 / 1024 / 1024) % 1024;
-        let petabytes = (size / 1024 / 1024 / 1024 / 1024 / 1024) % 1024;
-
-        let out = [];
-        if (petabytes > 1) return petabytes.toFixed(2) + " pb";
-        if (terabytes > 1) return terabytes.toFixed(2) + " tb";
-        if (gigabytes > 1) return gigabytes.toFixed(2) + " gb";
-        if (megabytes > 1) return megabytes.toFixed(2) + " mb";
-        if (kilobytes > 1) return kilobytes.toFixed(2) + " kb";
-        if (bytes > 1) return bytes.toFixed(2) + " b";
-        return "0 zero size";
-    }
 })();
